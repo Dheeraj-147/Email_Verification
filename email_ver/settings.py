7ef09b306f3c401bfb8cd1d774fb31041767c9a9
Dheeraj-147 <105336260+Dheeraj-147@users.noreply.github.com>
2024-10-08 01:56:10 +0530

    sett

--- email_ver/settings.py
+++ email_ver/settings.py
@@ -43,8 +43,8 @@ AUTHENTICATION_BACKENDS = [
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'front/build'),
 ]
