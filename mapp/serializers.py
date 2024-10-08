5c134111e00db0c56ffe8a634658d0707c19e59c
Dheeraj-147 <105336260+Dheeraj-147@users.noreply.github.com>
2024-10-08 09:14:07 +0530

    seria

--- mapp/serializers.py
+++ mapp/serializers.py
@@ -2,19 +2,31 @@ from rest_framework import serializers
 from .models import User
 
 class RegistrationSerializer(serializers.ModelSerializer):
+    cpassword = serializers.CharField(write_only=True)
+    profile_picture = serializers.ImageField(required=False)
+
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'password', 'profile_picture']
+        fields = ['id', 'username', 'email', 'password', 'cpassword', 'img']
         extra_kwargs = {'password': {'write_only': True}}
 
+    def validate(self, data):
+        if data['password'] != data['cpassword']:
+            raise serializers.ValidationError({"password": "Passwords do not match"})
+        if User.objects.filter(email=data['email']).exists():
+            raise serializers.ValidationError({"email": "Email is already registered"})
+        return data
+
     def create(self, validated_data):
-        profile_picture = validated_data.pop('profile_picture', None)
+        validated_data.pop('cpassword')
+        img = validated_data.pop('img', None)
         user = User.objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
             password=validated_data['password']
         )
-        if profile_picture:
-            user.profile_picture = profile_picture
-            user.save()
+        if img:
+            user.img = img
+        user.is_active = False
+        user.save()
         return user
