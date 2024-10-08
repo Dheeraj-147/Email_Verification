ae80a50d25963cc2cb047279a7c8e2b716692a13
Dheeraj-147 <105336260+Dheeraj-147@users.noreply.github.com>
2024-10-08 12:20:06 +0530

    urls

--- email_ver/urls.py
+++ email_ver/urls.py
@@ -8,7 +8,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('registration/', include('mapp.urls')),
     path('',TemplateView.as_view(template_name='index.html'),name='home'),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] 
 
 if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
